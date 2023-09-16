f1ab2bb5739eeff8e30fbc826cea264fb2005a70
Guilherme Costa <guilhermecosta.ds1@gmail.com>
2023-09-15 21:58:47 -0300

    Initial commit

---
+++ src/test/java/com/apirest/bootcampsantander/BootcampSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.bootcampsantander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
